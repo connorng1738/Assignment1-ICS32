548d8eda8aa143939255732d774aeea63c0acfc6
Connor Ng <ngce@uci.edu>
2025-04-06 23:30:58 -0700

    Second commit - worked on Command C

--- a1-diary-starter/a1.py
+++ a1-diary-starter/a1.py
@@ -5,11 +5,21 @@
     # Connor Ng
     # ngce@uci.edu
     # ngce
+    # Command C Test Input: 
+    # C "/home/john/ics 32/my notebooks" -n my_diary
+
 import command_parser
+def main():
+    while True:
+        command_str = input("")
+        print(command_str)
+        if command_str == "Q":
+            break
+        if command_str.startswith("C "):
+                command_parser.create_notebook(command_str)
 
 if __name__=="__main__":
-    
-    command_str= input('Input your command string here:\n').strip()
-    if command_str.startswith("C "):
-        command_parser.create_notebook(command_str)
+    main()
+
+
     
--- a1-diary-starter/command_parser.py
+++ a1-diary-starter/command_parser.py
@@ -6,18 +6,62 @@
 # ngce@uci.edu
 # ngce
 import shlex
+from pathlib import Path
+from notebook import Notebook
+import json
 
 def create_notebook(command):
     
     info = shlex.split(command)
     print(info)
 
-    username = input("")
-    password = input("")
-    bio = input("")
+    path = info[1]
+    print(f"Path{path}")
+    notebook_name = info[3]
+    print(f"Notebook_name{notebook_name}")
+    print(notebook_name)
+    
+    directory = Path(path)
+    if not directory.exists() or not directory.is_dir():
+        print("ERROR")
+        return
+
+    notebook_path = directory / f"{notebook_name}.json"
+    if notebook_path.exists():
+        print("ERROR")
+        return
+
+    username = input("Please enter your username:\n")
+    password = input("Please enter your password:\n")
+    bio = input("Please describe your bio:\n")
+
+    notebook = Notebook(username, password, bio)
+    
+    notebook_data = {
+        'username': notebook.username,
+        'password': notebook.password,
+        'bio': notebook.bio,
+    }
+
+    with open(notebook_path, 'w') as f:
+        json.dump(notebook_data, f, indent = 4)
+        print("Created")
 
     
-
     print(username, password, bio)
+
+
+def delete_notebook(command):
+    pass
+
+def load_notebook(command):
+    pass
+
+def edit_notebook(command):
+    pass
+
+def print_notebook_info(command):
+    pass
+
 
 
